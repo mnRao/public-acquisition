89ee49f56051a98d2bf6bf9b0438f491b7c57078
serghei.golub <serghei.golub@1d2ba7c6-fc39-71b3-1e0d-c5e47b1bee05>
2013-03-18 15:58:36 +0000

    Current backup 18.03.2013 - DAILY FINAL

---
+++ PublicAcquisitions/src/main/java/com/daac/pacq/domain/ref/TenderStatus.java
@@ -0,0 +1,10 @@
+package com.daac.pacq.domain.ref;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "REF_TENDER_STATUS")
+public class TenderStatus extends AbstractRefEntity  {
+	
+}
